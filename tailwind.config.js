87f8dc3fee76c908280a37a4605a9a3459e06ffe
Daniel Hrečin <hrecind@caliaq.dev>
2025-05-20 14:00:42 +0200

    Add Tailwind CSS configuration file

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",  // Všechny soubory JS, TS, JSX, TSX v adresáři src
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
